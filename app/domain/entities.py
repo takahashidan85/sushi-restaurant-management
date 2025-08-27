e147df5ba0b7e32b23524f183e4a1d989a963600
takahashidan85 <catdangtran1@gmail.com>
2025-08-27 13:50:53 +0700

    remove file

--- app/domain/entities.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-@dataclass
-class MenuItemEntity:
-    id: int = None
-    name: str = ""
-    price: float = 0.0
-    category: str = ""
