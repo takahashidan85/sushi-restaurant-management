6269dcf4c3be2d3ef3d0a6f0a95720cbbbce4408
takahashidan85 <catdangtran1@gmail.com>
2025-08-27 14:09:44 +0700

    modify

--- app/presentation/__init__.py
+++ app/presentation/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from .customer_route import customer_bp
+from .sushi_item_route import sushi_item_bp
+from .order_route import order_bp
+from .order_detail_route import order_detail_bp
 
-def register_blueprints(app: Flask):
-    from .menu_routes import bp as menu_bp
-    from .order_routes import bp as order_bp
-    app.register_blueprint(menu_bp, url_prefix='/api/menu')
-    app.register_blueprint(order_bp, url_prefix='/api/orders')
+def register_blueprints(app):
+    app.register_blueprint(customer_bp, url_prefix="/customers")
+    app.register_blueprint(sushi_item_bp, url_prefix="/sushi_items")
+    app.register_blueprint(order_bp, url_prefix="/orders")
+    app.register_blueprint(order_detail_bp, url_prefix="/order_details")
