ca094ce212094ed925eb945184582f4ec462dc36
takahashidan85 <catdangtran1@gmail.com>
2025-08-28 00:08:14 +0700

    update for migration and database init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flasgger import Swagger
 from .presentation import register_blueprints
+from .extensions import db, migrate
+from .config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     Swagger(app)
     register_blueprints(app)
+    
     return app
--- app/config.py
+++ app/config.py
@@ -3,7 +3,9 @@ import os
 class Config:
     DEBUG = True
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://sa:Ahihi1234!@localhost\\SQLEXPRESS/SushiDB?driver=ODBC+Driver+17+for+SQL+Server"
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///sushi.db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
