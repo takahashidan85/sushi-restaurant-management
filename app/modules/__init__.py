198cf04ba37e1e94f0c1c34afbd3b73d479c7364
takahashidan85 <catdangtran1@gmail.com>
2025-09-03 07:26:48 +0700

    add __init__ for modules

---
+++ app/modules/__init__.py
@@ -0,0 +1,6 @@
+from app.modules.customer.presentation import customer_bp
+from app.modules.order.presentation import order_bp
+from app.modules.sushi_item.presentation import sushi_item_bp
+from app.modules.order_detail.presentation import order_detail_bp
+
+__all__ = ["customer_bp", "order_bp", "sushi_item_bp", "order_detail_bp"]
