8f291c4a0f428072c048fa60c418a675df832828
takahashidan85 <catdangtran1@gmail.com>
2025-09-10 22:17:50 +0700

    add logging handler and ping for test

--- app/app_factory.py
+++ app/app_factory.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from app.core.config import Config
 from app.core.extensions import db, migrate
 from app.core.error_handler import register_error_handlers
-from app.core.logging import configure_logging
+from app.core.logging import configure_logging, setup_request_logging
 from app.modules import (
     customer_bp,
     order_bp,
@@ -30,14 +30,9 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    #Import models for migrations
-    from app.modules.customer.infrastructure import models as customer_models
-    from app.modules.order.infrastructure import models as order_models
-    from app.modules.sushi_item.infrastructure import models as sushi_item_models
-    from app.modules.order_detail.infrastructure import models as order_detail_models
-
     # Logging
     configure_logging(app)
+    setup_request_logging(app)
 
     # Error handlers
     register_error_handlers(app)
@@ -52,6 +47,11 @@ def create_app(config_class=Config):
     app.register_blueprint(order_detail_bp)
 
     app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+    """Health check to test if API works."""
+    @app.route("/ping")
+    def ping():
+        return {"message": "pong"}, 200
 
     return app
 
